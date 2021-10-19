5b74446486401b6baf50efc684d54eaff201b80d
YangGwangmo <kuyang95@naver.com>
2021-10-19 17:31:38 +0900

    set Large Icon

--- app/src/main/java/com/pang/howwasyourday/MainActivity.kt
+++ app/src/main/java/com/pang/howwasyourday/MainActivity.kt
@@ -3,6 +3,8 @@ package com.pang.howwasyourday
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,8 +16,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        // notification 빌더 (아이콘, 텍스트등 설정 여기서 함)
         var builder = NotificationCompat.Builder(this, createNotificationChannel("first", "mainNoti"))
             .setSmallIcon(android.R.drawable.ic_menu_call)
+            .setLargeIcon(createBitmapIcon(R.drawable.mouse_large))
             .setContentTitle("엄마")
             .setContentText("오늘 하루 어땠어?")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
@@ -26,6 +31,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    private fun createBitmapIcon(imageRes : Int) : Bitmap {
+        val mouse_largeIcon : Bitmap = BitmapFactory.decodeResource(getResources(), imageRes)
+        return mouse_largeIcon
+    }
+
+    // 오레오 이상에서는 노티에 채널 필요해서 생성해줘야 함
     private fun createNotificationChannel(id: String, name: String) : String {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
@@ -36,6 +48,7 @@ class MainActivity : AppCompatActivity() {
       return id
     }
 
+    // 노티 발송
     private fun pushNotify(builder : NotificationCompat.Builder) = with(NotificationManagerCompat.from(this)){
         notify(1, builder.build())
     }

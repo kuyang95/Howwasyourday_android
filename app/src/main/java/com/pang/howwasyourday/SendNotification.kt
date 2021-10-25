c773d25adfe9f87ed30210b0ea0c16eafd575eba
YangGwangmo <kuyang95@naver.com>
2021-10-25 18:56:49 +0900

    add notification reply action

--- app/src/main/java/com/pang/howwasyourday/SendNotification.kt
+++ app/src/main/java/com/pang/howwasyourday/SendNotification.kt
@@ -2,10 +2,14 @@ package com.pang.howwasyourday
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.RemoteInput
 
 
 fun sendNotification(context: Context) {
@@ -13,6 +17,29 @@ fun sendNotification(context: Context) {
 
     lateinit var manager : NotificationManager
     lateinit var channel: NotificationChannel
+
+    val remoteInput = RemoteInput.Builder("key_text_reply")
+        .build()
+
+    val resultIntent = Intent(context, NotificationReceiver::class.java)
+
+    val resultPendingIntent = PendingIntent.getBroadcast(
+        context,
+        0,
+        resultIntent,
+        0
+    )
+
+    val action : NotificationCompat.Action = NotificationCompat.Action.Builder(
+    R.drawable.mouse_large,
+        "Reply",
+        resultPendingIntent)
+        .addRemoteInput(remoteInput)
+        .setAllowGeneratedReplies(true)
+        .build()
+
+
+
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
         manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
@@ -29,8 +56,21 @@ fun sendNotification(context: Context) {
         .setContentTitle("엄마")
         .setContentText("오늘 하루 어땠어?")
         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .addAction(action)
         .setAutoCancel(false)
 
     NotificationManagerCompat.from(context).notify(1,builder.build())
 
+}
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+       val remoteInput = RemoteInput.getResultsFromIntent(p1)
+
+        if (remoteInput != null){
+            val title = remoteInput.getCharSequence(
+                "key_text_reply")
+
+        }
+    }
 }

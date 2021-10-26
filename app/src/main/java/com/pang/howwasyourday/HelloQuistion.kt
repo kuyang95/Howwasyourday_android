2779b3fd3b1f8f6888a3da3a14a39b3451e0cce7
YangGwangmo <kuyang95@naver.com>
2021-10-26 20:21:35 +0900

    implemented Hello Quiz Activity

---
+++ app/src/main/java/com/pang/howwasyourday/HelloQuistion.kt
@@ -0,0 +1,53 @@
+package com.pang.howwasyourday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.pang.howwasyourday.databinding.ActivityHelloQuistionBinding
+import com.pang.howwasyourday.databinding.ActivityMainBinding
+
+class HelloQuistion : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHelloQuistionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_hello_quistion)
+
+        // view Vinding 사용해서 view 접근
+        binding = ActivityHelloQuistionBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.et1.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+                if(binding.et1.text.toString().equals("")){
+                    binding.enterButton.visibility = View.INVISIBLE
+                }
+                else{
+                    binding.enterButton.visibility = View.VISIBLE
+                    }
+
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+        })
+
+
+
+
+        
+    }
+}
